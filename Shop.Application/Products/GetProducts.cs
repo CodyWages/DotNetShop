b9dc535db030c6c1bfbe5c9a472bd940c859dc55
CodyWages <segawydoc@gmail.com>
2022-11-20 20:34:23 -0600

    Add project files.

---
+++ Shop.Application/Products/GetProducts.cs
@@ -0,0 +1,34 @@
+﻿using Shop.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Products
+{
+    public class GetProducts
+    {
+        private ApplicationDbContext _ctx;
+
+        public GetProducts(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<ProductViewModel> Do() =>
+            _ctx.Products.ToList().Select(x => new ProductViewModel
+            {
+                Name = x.Name,
+                Description = x.Description,
+                Value = $"$ {x.Value.ToString("N2")}" // 1000.10 => 1,000.10
+            });
+
+        public class ProductViewModel
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
