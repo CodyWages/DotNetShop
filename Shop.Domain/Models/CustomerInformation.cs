13d5d9e54d3b98d628d232cff4542f7fe3eda7ad
CodyWages <segawydoc@gmail.com>
2022-12-07 21:54:20 -0600

    Fixed Order Management Issue

--- Shop.Domain/Models/CustomerInformation.cs
+++ Shop.Domain/Models/CustomerInformation.cs
@@ -13,6 +13,7 @@ namespace Shop.Domain.Models
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+
         public string Address1 { get; set; }
         public string Address2 { get; set; }
         public string City { get; set; }
--- Shop.UI/Pages/Admin/OrderManagement.cshtml.cs
+++ Shop.UI/Pages/Admin/OrderManagement.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Shop.UI.Pages.Admin
 {
-    public class Index1Model : PageModel
+    public class OrdersModel : PageModel
     {
         public void OnGet()
         {
