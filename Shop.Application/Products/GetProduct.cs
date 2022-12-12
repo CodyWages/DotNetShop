2f5c5205d7e7fcafed664b98dcb676684a8f9227
CodyWages <segawydoc@gmail.com>
2022-12-11 19:46:00 -0600

    Added Low Stock / Out of Stock Functionality

--- Shop.Application/Products/GetProduct.cs
+++ Shop.Application/Products/GetProduct.cs
@@ -49,7 +49,7 @@ namespace Shop.Application.Products
                     {
                         Id = y.Id,
                         Description = y.Description,
-                        InStock = y.Qty > 0,
+                        Qty = y.Qty,
                     })
                 })
                 .FirstOrDefault();
@@ -67,7 +67,7 @@ namespace Shop.Application.Products
         {
             public int Id { get; set; }
             public string Description { get; set; }
-            public bool InStock { get; set; }
+            public int Qty { get; set; }
         }
     }
 }
--- Shop.Application/Products/GetProducts.cs
+++ Shop.Application/Products/GetProducts.cs
@@ -1,4 +1,5 @@
-﻿using Shop.Database;
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,18 +18,23 @@ namespace Shop.Application.Products
         }
 
         public IEnumerable<ProductViewModel> Do() =>
-            _ctx.Products.ToList().Select(x => new ProductViewModel
-            {
-                Name = x.Name,
-                Description = x.Description,
-                Value = $"${x.Value.ToString("N2")}" // 1000.10 => 1,000.10
-            });
+            _ctx.Products
+                .Include(x => x.Stock)
+                .Select(x => new ProductViewModel
+                {
+                    Name = x.Name,
+                    Description = x.Description,
+                    Value = $"${x.Value.ToString("N2")}", // 1000.10 => 1,000.10
+                    StockCount = x.Stock.Sum(y => y.Qty),
+                })
+                .ToList();
 
         public class ProductViewModel
         {
             public string Name { get; set; }
             public string Description { get; set; }
             public string Value { get; set; }
+            public int StockCount { get; set; }
         }
     }
 }
